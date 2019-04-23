1e55b87770b1e810895e574fc770eaacbf2f9f7e
randomblame <dumbandroid@gmail.com>
2019-04-23 16:40:09 -0700

    llvm-lto dtbo fixup

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -35,21 +35,26 @@ TARGET_USES_UEFI := true
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0xa90000 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=2048 firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7 androidboot.usbcontroller=a600000.dwc3 androidboot.selinux=permissive
 
-BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+BOARD_KERNEL_IMAGE_NAME := Image-dtb
+TARGET_COMPILE_WITH_MSM_KERNEL := true
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_RAMDISK_OFFSET := 0x01000000
 TARGET_KERNEL_ARCH := arm64
 NEED_KERNEL_MODULE_SYSTEM := true
+TARGET_NEEDS_DTBOIMAGE := true
 ifeq ($(TARGET_PREBUILT_KERNEL),)
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
-#TARGET_KERNEL_SOURCE := kernel/xiaomi/sm8150
-TARGET_KERNEL_SOURCE := kernel/xiaomi/4.19
+TARGET_KERNEL_SOURCE := kernel/xiaomi/sm8150
 endif
 
 # Platform
 TARGET_BOARD_PLATFORM := sm8150
+
+# vbmeta
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
 
 # Audio
 AUDIO_FEATURE_ENABLED_AAC_ADTS_OFFLOAD := true
